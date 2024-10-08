a0e7a2c98ed8d7c1378ce06c93337ef5d9d3b318
no <no>
2024-10-08 16:56:58 -0700

    enforce constant output rate of 125Hz

--- src/Arduino/main/main.ino
+++ src/Arduino/main/main.ino
@@ -5,6 +5,7 @@
 #include <Wire.h>
 
 #define PRINT_FLOAT_DECIMALS 4
+#define LOOP_PERIOD_MICROS 8000
 
 Adafruit_MPU6050 mpu;
 
@@ -14,7 +15,6 @@ void setup(void) {
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
   }
 
-  // Try to initialize!
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -26,12 +26,16 @@ void setup(void) {
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-  Serial.println("Timestamp,AccelX,AccelY,AccelZ,GyroX,GyroY,GyroZ");
-
   delay(100);
 }
 
-void loop() {
+void output_data() {
+  /* Print out header */
+  static bool first_run = true;
+  if (first_run) {
+    first_run = false;
+    Serial.println("Timestamp,AccelX,AccelY,AccelZ,GyroX,GyroY,GyroZ");
+  }
 
   /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
@@ -54,4 +58,21 @@ void loop() {
   Serial.print(",");
   Serial.print(g.gyro.z, PRINT_FLOAT_DECIMALS);
   Serial.println("");
+}
+
+void loop() {
+  /* Get starting time */
+  unsigned long start_time = micros();
+
+  /* Output data */
+  output_data();
+
+  /* Get ending time */
+  unsigned long end_time = micros();
+
+  /* Enforce constant output rate */
+  unsigned long duration = end_time - start_time;
+  if (duration < LOOP_PERIOD_MICROS) {
+    delayMicroseconds(LOOP_PERIOD_MICROS - duration);
+  }
 }
