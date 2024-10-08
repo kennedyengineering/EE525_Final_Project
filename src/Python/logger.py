afbc42d88b0b69a9c59418fcbb973c5d32d9a375
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-10-08 14:55:16 -0700

    Updated timestamp to include microseconds, but will change so Arudino code provides it

--- src/Python/logger.py
+++ src/Python/logger.py
@@ -3,24 +3,34 @@ import csv
 import serial
 from serial.serialutil import SerialException
 
+from datetime import datetime
 import time
 import argparse
 import os
 
-# Create timestamp to log entries and create log file
-date_str = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+# Constants
+CSV_COLUMNS = ["Timestamp", "AccelX", "AccelY", "AccelZ", "GyroX", "GyroY", "GyroZ"]
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", type=str, default="/dev/ttyACM0")
 parser.add_argument("--baud_rate", type=int, default=115200)
 parser.add_argument("--log_dir", type=str, default="./logs")
+date_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
 parser.add_argument("--csv_file_name", type=str, default=f"imu_data_{date_str}.csv")
 args = parser.parse_args()
 
-# Create log directory if it does not exist
+# Ensure the log directory exists
 if not os.path.exists(args.log_dir):
     os.makedirs(args.log_dir)
+
+# Create a .gitignore file inside the log directory with specific content
+gitignore_path = os.path.join(args.log_dir, ".gitignore")
+
+# Check if the .gitignore file already exists, and if not, create it
+if not os.path.exists(gitignore_path):
+    with open(gitignore_path, "w") as f:
+        f.write("*\n!.gitignore\n")
 
 # Full path to the CSV file
 file_path = os.path.join(args.log_dir, args.csv_file_name)
@@ -32,18 +42,17 @@ except SerialException as ex:
     print(ex.strerror)
     exit()
 
-# Allow for Arduino to reset (physically press the reset button)
+# Allow for Arduino to reset (physically press the reset button - for MacOS)
 time.sleep(3)
 ser.reset_input_buffer()
 
 # Read data from Arduino
 try:
-    print(f"Starting logger. Writing to {file_path}")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    print(f"Starting logger {timestamp}. Writing to {file_path}")
     with open(file_path, "w", newline="") as csv_log_file:
         writer = csv.writer(csv_log_file)
-        writer.writerow(
-            ["Timestamp", "AccelX", "AccelY", "AccelZ", "GyroX", "GyroY", "GyroZ"]
-        )
+        writer.writerow(CSV_COLUMNS)
 
         while True:
             if ser.in_waiting > 0:  # Check if there is data to read, if so read it
@@ -58,7 +67,7 @@ try:
                     ]
 
                     # Get the current timestamp
-                    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 
                     # Write the timestamp and parsed data to the CSV file
                     writer.writerow([timestamp] + parsed_imu_data)
