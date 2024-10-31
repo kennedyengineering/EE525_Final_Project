b3ff2943bde88126f6a37e0592ff77b19e9ac0d6
no <no>
2024-10-31 12:14:48 -0700

    plots autocorrelation

--- src/Matlab/plot_noise_autocorrelation.m
+++ src/Matlab/plot_noise_autocorrelation.m
@@ -1,4 +1,4 @@
-% Compute autocorrelation
+% Characterize noise
 
 clear; clc;
 
@@ -13,71 +13,75 @@ end
 % Read data from log file into a table
 data = readtable(dataFilePath);
 
-% Identify accelerometer and gyroscope data
+% Identify relavant data
+timeIdx = contains(data.Properties.VariableNames, 'Time');
+time = data{:, timeIdx};
+
 accelIdx = contains(data.Properties.VariableNames, 'Accel');
 accelTable = data(:, accelIdx);
 
 gyroIdx = contains(data.Properties.VariableNames, 'Gyro');
 gyroTable = data(:, gyroIdx);
 
-% Define constants
-dt = 0.008;
+% Isolate the noise
+accelNoiseTable = accelTable - mean(accelTable);
+gyroNoiseTable = gyroTable - mean(gyroTable);
+
+% Sample period
+dt = 0.008; % seconds per sample
 
 % Number of lags
 max_tau = 60;  % Maximum delay in seconds
 max_lag = floor(max_tau / dt);  % Maximum lag in number of samples
-lags = -max_lag:max_lag;  % Lags for autocorrelation estimation
 
-% Preallocate the autocorrelation array
-R_estimated = zeros(size(lags));
+% Analyze accel noise
+for table={accelNoiseTable}
 
-% Plot accelerometer autocorrelations
-for table={accelTable, gyroTable}
-
-    % Create new figure
+    % Plot noise
     figure;
+    sgtitle('Accelerometer Noise');
     hold on;
     subplotcount = 1;
 
     for entry=table{1}
 
-        % Calculate the autocorrelation
-        X = entry{:, :};
+        % Plot on subplot
+        subplot(width(table{1}), 1, subplotcount);
+        subplotcount = subplotcount + 1;
 
-        R_estimated = xcov(X, max_lag) / var(X);
-
-        % Plot autocorrelation
         X_name = entry.Properties.VariableNames(1);
 
-        subplot(width(table{1}), 2, subplotcount);
-        subplotcount = subplotcount + 1;
-        plot(lags * dt, R_estimated, 'LineWidth', 1.5);
-        xlabel('\tau');
-        ylabel(sprintf('R_{%s}(\\tau)', string(X_name)));
-        title(strcat(X_name, ' Autocorrelation'));
+        plot(time, entry{:, :}, 'LineWidth', 1.5);
+        title(strcat(X_name,' Noise'));
+        xlabel('Time (ms)');
+        xlim([time(1), time(end)]);
+        ylabel('Linear Acceleration (m/s^2)');
         grid on;
+    end
 
-        % Calculate the power spectral density
-        % https://www.mathworks.com/help/signal/ug/power-spectral-density-estimates-using-fft.html
-        R_estimated = R_estimated(2:end);
-        N = length(R_estimated);
-        fs = 1/dt;
+    % Plot autocorrelation
+    figure;
+    sgtitle('Accelerometer Noise Autocorrelation (Unbiased Estimate)');
+    hold on;
+    subplotcount = 1;
 
-        xdft = fft(R_estimated);
-        xdft = xdft(1:N/2+1);
-        psdx = (1/(fs*N)) * abs(xdft).^2;
-        psdx(2:end-1) = 2*psdx(2:end-1);
-        freq = 0:fs/N:fs/2;
+    for entry=table{1}
 
-        % Plot power spectral density
-        subplot(width(table{1}), 2, subplotcount);
+        % Compute autocorrelation
+        [r, lags] = xcorr(entry{:, :}, 'unbiased');
+        taus = lags * dt;   % Convert from samples to seconds
+
+        % Plot on subplot
+        subplot(width(table{1}), 1, subplotcount);
         subplotcount = subplotcount + 1;
-        plot(freq, pow2db(psdx), 'LineWidth', 1.5);
-        xlabel('Frequency (Hz)');
-        ylabel('Power/Frequency (dB/Hz)');
-        xlim([0, freq(end)]);
-        title(strcat(X_name,' Power Spectral Density'));
-        grid on;
 
+        X_name = entry.Properties.VariableNames(1);
+
+        plot(taus, r, 'LineWidth', 1.5);
+        title(strcat(X_name,' Autocorrelation'));
+        xlabel('\tau (s)');
+        xlim([taus(1), taus(end)]);
+        ylabel('Linear Acceleration (m/s^2)');
+        grid on;
     end
 end
