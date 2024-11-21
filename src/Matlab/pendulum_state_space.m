352215bb76d6c9ed4cd0db4458a28a65722f1a32
no <no>
2024-11-21 14:54:46 -0800

    made a method

--- src/Matlab/pendulum_state_space.m
+++ src/Matlab/pendulum_state_space.m
@@ -75,54 +75,60 @@ plot(time(1:end-1), d_theta, 'DisplayName', 'Angular Velocity (rad/s)');
 title('Observed Pendulum Angle and Angular Velocity');
 legend;
 
-% Parameters
-g = 9.81;  % Gravity (m/s^2)
+% Theoretical Parameters
+G = 9.81;  % Gravity (m/s^2)
 R = 0.4064;  % Length of pendulum (16 inches in meters)
-m = 0.073;  % Mass of pendulum (73g in kg)
+M = 0.073;  % Mass of pendulum (73g in kg)
 Ts = 1/30;  % Sampling time of 30 FPS video (s)
-b = 0.02;  % Damping coefficient (initial guess)
+B = 0.02;  % Damping coefficient (initial guess)
 
-% Continuous-time state-space matrices
-A_c = [0, 1; -g/R, -b/m];
-B_c = [0; 0];
-C_c = eye(2);  % Identity matrix
-D_c = [0; 0];
+function X = simulate_system(g, r, m, b, ts, duration, X0)
+    % Parameters
+    % g - gravity (m/s^2)
+    % r - radius of pendulum (m)
+    % m - mass of pendulum (kg)
+    % ts - sample time (s)
+    % b - damping coefficient
+    % X - state (rad, rad/s)
+    % X0 - initial state (rad, rad/s)
 
-% Discretization
-A_d = expm(A_c * Ts);
-B_d = integral(@(t) expm(A_c * t) * B_c, 0, Ts, 'ArrayValued', true);
-C_d = C_c;
-D_d = D_c;
+    % Continuous-time state-space matrices
+    A_c = [0, 1; -g/r, -b/m];
+    B_c = [0; 0];
+    C_c = eye(2);  % Identity matrix
+    D_c = [0; 0];
 
-% Display results
-disp('Continuous-Time A Matrix:'); disp(A_c);
-disp('Discrete-Time A Matrix:'); disp(A_d);
-disp('Discrete-Time B Matrix:'); disp(B_d);
-disp('C Matrix:'); disp(C_d);
-disp('D Matrix:'); disp(D_d);
+    % Discretization
+    A_d = expm(A_c * ts);
+    B_d = integral(@(t) expm(A_c * t) * B_c, 0, ts, 'ArrayValued', true);
+    C_d = C_c;
+    D_d = D_c;
+
+    % Initial conditions
+    X = zeros(2, duration);
+    X(:, 1) = X0;
+
+    for k = 1:duration - 1
+        X(:, k+1) = A_d * X(:, k);  % No input since B_d * u = 0
+    end
+end
 
 % Simulating the system (optional)
-% Initial conditions
-x0 = [theta(1); d_theta(1)];  % Initial angle (radians) and angular velocity (rad/s)
-x = zeros(2, length(time));
-x(:, 1) = x0;
-
-for k = 1:length(time) - 1
-    x(:, k+1) = A_d * x(:, k);  % No input since B_d * u = 0
-end
+x0 = [theta(1); d_theta(1)];
+dt = time(2);
+duration = length(time);
+x = simulate_system(G, R, M, B, dt, duration, x0);
 
 % Plot results
 figure;
-plot(time, x(1, :), 'r', 'DisplayName', 'Theta (rad)'); % Angular displacement
+title('Discrete-Time Simulation of Pendulum');
 hold on;
+plot(time, x(1, :), 'r', 'DisplayName', 'Theta (rad)'); % Angular displacement
 plot(time, x(2, :), 'b', 'DisplayName', 'Angular Velocity (rad/s)'); % Angular velocity
-
-% Set up labels and legend
 xlabel('Time (s)');
 ylabel('State');
 legend;
 grid on;
-title('Discrete-Time Simulation of Pendulum');
 
 % TODO
 % use initial guess of beta to start optimization
