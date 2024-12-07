a73adaa2e44e718d82a8da66b62ca4d396d357e6
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-12-07 09:27:28 -0800

    Added complementary figure for the angle

--- src/Matlab/complementary_filter.m
+++ src/Matlab/complementary_filter.m
@@ -52,11 +52,50 @@ gyroX = data{:, matches(data.Properties.VariableNames, 'GyroX')}';
 gyroY = data{:, matches(data.Properties.VariableNames, 'GyroY')}';
 gyroZ = data{:, matches(data.Properties.VariableNames, 'GyroZ')}';
 
-%% Define constants
-dt = 0.008;  % Sample period
-g = 9.80665; % Standard gravity
-b = 0.0190;  % Drag coefficient (optimal)
-m = 0.0837;  % Mass (optimal)
-r = 0.3628;  % Radius (optimal)
-
 %% Complementary Filter (with control input)
+% Initialize complementary filter parameters
+alpha = 0.96;  % High-pass filter coefficient for gyro
+complementary_angle = atan2(accelY(1), sqrt(accelX(1).^2 + accelZ(1).^2));  % Initialize with first accelerometer reading
+dt = 0.008;  % Your sampling period
+
+% Arrays to store results
+num_samples = length(time);
+complementary_angles = zeros(1, num_samples);
+angular_velocities = zeros(1, num_samples);
+
+% Store initial values
+complementary_angles(1) = complementary_angle;
+angular_velocities(1) = gyroX(1);
+
+% Run complementary filter
+for k = 2:num_samples
+    % Predict angle using gyroscope data
+    gyro_angle = complementary_angles(k-1) + gyroX(k)*dt;
+
+    % Get accelerometer angle
+    accel_angle = atan2(accelY(k), sqrt(accelX(k)^2 + accelZ(k)^2));
+
+    % Combine using complementary filter
+    complementary_angles(k) = alpha * gyro_angle + (1-alpha) * accel_angle;
+
+    % Store angular velocity (could be filtered if desired)
+    angular_velocities(k) = gyroX(k);
+end
+
+% Plot results
+figure;
+subplot(2,1,1);
+plot(time, complementary_angles, 'DisplayName', 'Angle');
+title('Complementary Filter - Angle Estimation');
+ylabel('Angle (rad)');
+xlabel('Time (s)');
+legend;
+grid on;
+
+subplot(2,1,2);
+plot(time, angular_velocities, 'DisplayName', 'Angular Velocity');
+title('Angular Velocity');
+ylabel('Angular Velocity (rad/s)');
+xlabel('Time (s)');
+legend;
+grid on;
